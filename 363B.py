f8199fa949a1a2789dbbf5de20b74adbab1d87cc
Eduardo Augusto Almeida lira <eduz92337@gmail.com>
2025-01-16 15:37:39 -0300

    Update 363B.py

--- 363B.py
+++ 363B.py
@@ -1,17 +1,14 @@
-def muros(n_k,sequencia):
-    sequencia = [int(c) for c in sequencia]
-    n_k = [int(c) for c in n_k]
-    n,k = n_k[0],n_k[1]
-    somas = 0
-    posição = 0
-    for i in range(0,n-k+1):
-        soma = sum(sequencia[i:i+k])
-        if soma < somas :
-            posição = i
-        if soma >= somas:
-            somas = soma
-    return posição+1
-        
-print(muros(n_k=input().split(),sequencia=input().split()))
+n,k = list(map(int,input().split()))
+h = list(map(int,input().split()))
+v = float('inf')
+indice = len(h)
+soma_atual = sum(h[:k])
+v = soma_atual
+indice = 1
+for i in range(1, n - k + 1):
+    soma_atual = soma_atual - h[i - 1] + h[i + k - 1]
+    if soma_atual < v:
+        v = soma_atual
+        indice = i + 1
 
-
+print(indice)
